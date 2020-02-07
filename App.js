82b2c1da38ab0e3748c35f0c71f58ca794e1f9ec
jose <jose.barrios@nuestrodiario.com.gt>
2020-02-07 14:57:26 -0600

    add implement fix dependence react-native-implements

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
-
+import { Button } from 'react-native-elements';
 class App extends Component {
   render() {
     return(
       <Home>
         <Header />
-        <Text>Hola</Text>
+        <Text>Hola</Text> 
+        <Button 
+        title='BUTTON' />
       </Home>
     );
   }
