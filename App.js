538c87e9ea7e58edd031231d6947b8f6220ee989
cosoriond <christian.osorio@nuestrodiario.com.gt>
2020-02-07 11:10:10 -0600

    Component App

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
 
 class App extends Component {
   render() {
     return(
-      <View>
+      <Home>
+        <Header />
         <Text>Hola</Text>
-      </View>
+      </Home>
     );
   }
 }
+
+// const AppNavigator = createStackNavigaion({
+//     Home: App
+// });
+
+// const AppContainer = createAppContainer(AppNavigator);
 
 export default App;
